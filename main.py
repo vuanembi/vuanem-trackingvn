7ea0d74295f98f43930f22f6dcfd59f4c3b28e6c
HM <hieumdd@gmail.com>
2022-06-28 08:46:52 +0700

    feat: Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 from typing import Any
 
-from coffeehr import coffeehr_controller
-from tasks.tasks_service import create_tasks_service
+from trackingvn import trackingvn_controller
 
 
 def main(request):
     data: dict[str, Any] = request.get_json()
     print(data)
 
-    if "tasks" in data:
-        response = create_tasks_service()
-    elif "table" in data:
-        response = coffeehr_controller.controller(data)
-    else:
-        raise ValueError(data)
+    response = trackingvn_controller.controller(data)
 
     print(response)
     return response
